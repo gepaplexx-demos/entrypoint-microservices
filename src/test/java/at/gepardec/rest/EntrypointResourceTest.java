afcd48e29dc30d70956668c973bdecdb7936de02
Mario Oberwalder <mario.oberwalder@gepardec.com>
2022-05-23 11:15:21 +0200

    GPX-350 basic test

--- src/test/java/at/gepardec/rest/EntrypointResourceTest.java
+++ src/test/java/at/gepardec/rest/EntrypointResourceTest.java
@@ -7,17 +7,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
-@TestHTTPEndpoint(EntrypointResource.class)
 class EntrypointResourceTest {
 
-    @Inject
-    EntrypointResource entrypoint;
-
-    @Inject
-    Logger Log;
-
+    @Test
+    public void zeroTtlGivesNoContentResponse() {
+        given()
+                .when().get("/start/0")
+                .then()
+                .statusCode(204);
+    }
 
 }

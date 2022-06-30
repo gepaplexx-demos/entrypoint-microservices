d88189b6f3a1f702b8767b3501e63c3875c08627
Alexander Schober <alex.schober1@gmx.at>
2022-06-30 15:41:18 +0200

    changed readynessCheck

--- src/main/java/at/gepardec/health/ReadinessCheck.java
+++ src/main/java/at/gepardec/health/ReadinessCheck.java
@@ -1,30 +1,18 @@
 package at.gepardec.health;
 
 
-import com.sun.management.OperatingSystemMXBean;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
-import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import java.lang.management.ManagementFactory;
 
 @Liveness
 @ApplicationScoped
 public class ReadinessCheck implements HealthCheck {
 
-    @Inject
-    Logger Log;
-
-    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-
     @Override
     public HealthCheckResponse call() {
-        double cpuLoad = osBean.getProcessCpuLoad();
-        Log.info("ReadinessCheck - CpuUsage: " + String.format("%.2f", cpuLoad));
-        boolean ready = cpuLoad < 0.8;
-        return HealthCheckResponse.named("EntrypointService").status(ready).build();
+        return HealthCheckResponse.named("EntrypointService").status(true).build();
     }
 }
